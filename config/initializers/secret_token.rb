9c8c81088460b557b8192fe1851d79816555897d
dobabeswe <chololoy@gmail.com>
2013-02-04 16:55:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaisaFinder::Application.config.secret_token = 'e6aac34b387754b2a4752173bcea096064d4e5a99270d595af5bf73b2e9a90aa3c94f7661606d809483f8fd4371400fd77ec86eeff31b661b0879df881720329'
